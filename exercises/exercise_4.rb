9000804e9ed6044a0e9f7d770ec5e698b16205a0
Ayesha <ayesha_04@yahoo.com>
2017-08-01 17:51:40 +0000

    used where clauses and loops to find stores with particular attributes

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,39 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+Store.create(
+  name: "Surrey",
+  annual_revenue: "224000",
+  mens_apparel: "f",
+  womens_apparel: "t"
+)
+
+ Store.create(
+  name: "Whistler",
+  annual_revenue: "1900000",
+  mens_apparel: "t",
+  womens_apparel: "f"
+)
+
+Store.create(
+  name: "Yaletown",
+  annual_revenue: "430000",
+  mens_apparel: "t",
+  womens_apparel: "t"
+)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: "t").each do |store|
+  puts "#{store.name}"
+  puts "#{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("annual_revenue < ? AND womens_apparel = ?", 1000000, "t").each do |store|
+  puts "#{store.name}"
+  puts "#{store.annual_revenue}"
+end
+

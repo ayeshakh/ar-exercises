05c39fcb6e50f8261ac8cb0d4822c3871f608095
Ayesha <ayesha_04@yahoo.com>
2017-08-01 18:07:31 +0000

    found total revenue, average revenue and no of stores with sales> 1M using where and count clause

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+@average_revenue = Store.sum(:annual_revenue)/Store.count
+puts "#{@average_revenue}"
+
+#Output the number of stores that are generating $1M or more in annual sales.
+#Hint: Chain together where and size (or count) Active Record methods.
+
+puts Store.where("annual_revenue > ? ", 1000000).count

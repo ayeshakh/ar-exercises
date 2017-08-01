1e87335df3d1977cf278bbd47deb3c1261a1ff94
Ayesha <ayesha_04@yahoo.com>
2017-08-01 16:53:55 +0000

    created Store tables in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,29 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store < ActiveRecord::Base
+end
+
+  Store.create(
+    name: "Burnaby",
+    annual_revenue: "300000",
+    mens_apparel: "f",
+    womens_apparel: "t"
+  )
+
+   Store.create(
+    name: "Richmond",
+    annual_revenue: "1260000",
+    mens_apparel: "f",
+    womens_apparel: "t"
+  )
+
+  Store.create(
+    name: 'Gastown',
+    annual_revenue: "190000",
+    mens_apparel: "t",
+    womens_apparel: "f"
+  )
+
+
+puts Store.count

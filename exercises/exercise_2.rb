eb73940b28665595b1f899b863d37fa0fec0817f
Ayesha <ayesha_04@yahoo.com>
2017-08-01 17:10:19 +0000

    gave stores instances by finding them and updated name of 1st store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+#puts "#{@store1.inspect}" #to see if i have selected the correct store
+
+@store2 = Store.find(2)
+#puts "#{@store2.inspect}"
+
+@store1.update(name: "Macy")
+#puts "#{@store1.inspect}"
